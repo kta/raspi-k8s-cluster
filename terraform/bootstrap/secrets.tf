8896f11bc5611c50e4b226dcbd2676bccb67838b
Sakakibara <h11404m@gmail.com>
2026-01-18 22:51:07 +0900

    Comment out repo-creds for public repository

--- terraform/bootstrap/secrets.tf
+++ terraform/bootstrap/secrets.tf
@@ -1,25 +1,26 @@
 # ArgoCD がプライベートリポジトリにアクセスするための認証情報
-resource "kubernetes_secret" "argocd_repo_creds" {
-  metadata {
-    name      = "repo-creds"
-    namespace = kubernetes_namespace.argocd.metadata[0].name
-
-    labels = {
-      "argocd.argoproj.io/secret-type" = "repo-creds"
-    }
-  }
-
-  data = {
-    type     = "git"
-    url      = "https://github.com"
-    username = var.github_username
-    password = var.github_token
-  }
-
-  type = "Opaque"
-
-  depends_on = [helm_release.argocd]
-}
+# Public リポジトリの場合は不要なのでコメントアウト
+# resource "kubernetes_secret" "argocd_repo_creds" {
+#   metadata {
+#     name      = "repo-creds"
+#     namespace = kubernetes_namespace.argocd.metadata[0].name
+#
+#     labels = {
+#       "argocd.argoproj.io/secret-type" = "repo-creds"
+#     }
+#   }
+#
+#   data = {
+#     type     = "git"
+#     url      = "https://github.com"
+#     username = var.github_username
+#     password = var.github_token
+#   }
+#
+#   type = "Opaque"
+#
+#   depends_on = [helm_release.argocd]
+# }
 
 # Atlantis 用の GitHub Secret（Atlantis デプロイ時に使用）
 resource "kubernetes_namespace" "atlantis" {
